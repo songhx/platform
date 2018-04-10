5a55d6a3c55bf9da544b03a6dcf915335aff147c
bjsonghongxu <sleeponthestar@163.com>
2018-04-10 11:34:23 +0800

    [modify]拼车

---
+++ platform-ecarpool-api/src/main/java/com/platform/dao/CarpoolChatMapper.java
@@ -0,0 +1,14 @@
+package com.platform.dao;
+
+import com.platform.entity.CarpoolChat;
+import com.platform.service.IBasicSetMapper;
+
+/**
+ * 拼车聊天数据接口
+ *
+ * @author bjsonghongxu
+ * @create 2018-04-10 11:02
+ **/
+public interface CarpoolChatMapper extends IBasicSetMapper<CarpoolChat> {
+
+}
---
+++ platform-ecarpool-api/src/main/java/com/platform/service/CarpoolChatService.java
@@ -0,0 +1,12 @@
+package com.platform.service;
+
+import com.platform.entity.CarpoolChat;
+
+/**
+ * 拼车聊天服务接口
+ *
+ * @author bjsonghongxu
+ * @create 2018-04-10 11:13
+ **/
+public interface CarpoolChatService extends IBasicSetMapper<CarpoolChat> {
+}
