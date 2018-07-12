5ef8c895863bf5898298b7c3683da0f7d65c1ac2
bjsonghongxu <sleeponthestar@163.com>
2018-07-12 16:06:26 +0800

    [Add]初始化目优优新分支

--- platform-ecarpool-api/src/main/java/com/platform/service/CarpoolCarService.java
+++
@@ -1,9 +0,0 @@
-package com.platform.service;
-
-import com.platform.entity.CarpoolCar;
-
-/**
- * Created by zuimeideshiguang on 18/2/13.
- */
-public interface CarpoolCarService extends IBasicSetMapper<CarpoolCar> {
-}
